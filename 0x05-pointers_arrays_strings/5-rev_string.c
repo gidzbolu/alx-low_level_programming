1fa5f65c13533488df4278b49e0eb642b6a728c4
Gidbolu <bgideon0439@gmail.com>
2023-07-26 18:21:30 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *rev_string - reverses a string
+ *@s: integer entered
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int a, b, temp;
+	/* a = start */
+	/*b = end */
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	a--;
+	for (b = 0; a > b; b++, a--)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+	}
+}
