d5b45ce84806325a0a7f0ed18ac8ec27d7f4c28f
Gidbolu <bgideon0439@gmail.com>
2023-07-26 17:59:11 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - swaps the values of 2 integers
+ *@a: first  integer entered
+ *@b: second integer entered
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+	*b = x;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 int main(void);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
